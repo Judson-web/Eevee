7e725dae967c469fdbaf0ee92ad628b906b849bb
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-10-17 19:11:56 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -90,7 +90,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🆂🅴🅰️🆁🅲🅷 🅰️🅶🅰️🅸🅽', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('𝖲𝖾𝖺𝗋𝖼𝗁 𝖠𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=''),
         ]
         ]
     return InlineKeyboardMarkup(buttons)
